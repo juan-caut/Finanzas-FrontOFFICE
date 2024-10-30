a6c356cae5117f3c5cede88dfbec96688520dda8
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-10-30 13:27:13 -0500

    tab panel 0.1v

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { TabpanelComponent } from './pages/tabpanel/tabpanel.component';
 
 export const routes: Routes = [
     {path:'home',component:PrincipalComponent},
     {path:'login',component:LoginComponent},
     {path:'signin',component:SignupComponent},
-    {path:'panel',component:PrincipalComponent},
+    {path:'panel',component:TabpanelComponent},
     {path:'**',pathMatch:'full',redirectTo:'login'},
 
 ];
