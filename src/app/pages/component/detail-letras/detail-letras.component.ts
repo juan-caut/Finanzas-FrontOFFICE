a33a831fa5eabf4dbfa60504ae6e0efd91da2961
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-11-05 20:17:56 -0500

    add letra details 1v

--- src/app/pages/component/detail-letras/detail-letras.component.ts
+++ src/app/pages/component/detail-letras/detail-letras.component.ts
@@ -3,7 +3,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 @Component({
@@ -12,29 +11,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   imports: [MatTableModule,MatPaginatorModule,MatCheckboxModule,MatExpansionModule],
   templateUrl: './detail-letras.component.html',
   styleUrl: './detail-letras.component.css',
-  animations: [
-    trigger('transitionMessages', [
-      state('void', style({ opacity: 0 })),
-      state('enter', style({ opacity: 1 })),
-      transition('void => enter', [
-        animate('300ms ease-in')
-      ]),
-      transition('enter => void', [
-        animate('300ms ease-out')
-      ])
-    ])
-  ]
 })
 
 
 
 export class DetailLetrasComponent {
 
-
-
+  readonly panelOpenState = signal(true);
   
-  readonly panelOpenState = signal(false);
+  readonly panelOpenState2 = signal(false);
 
 
-  
 }
