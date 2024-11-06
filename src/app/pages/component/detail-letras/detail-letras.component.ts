fe53c014ab7747141d755185534e4be0bf7135d1
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-11-05 19:35:32 -0500

    datepicker regist letras

--- src/app/pages/component/detail-letras/detail-letras.component.ts
+++ src/app/pages/component/detail-letras/detail-letras.component.ts
@@ -1,17 +1,40 @@
-
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'app-detail-letras',
   standalone: true,
-  imports: [MatTableModule,MatPaginatorModule,MatCheckboxModule],
+  imports: [MatTableModule,MatPaginatorModule,MatCheckboxModule,MatExpansionModule],
   templateUrl: './detail-letras.component.html',
-  styleUrl: './detail-letras.component.css'
+  styleUrl: './detail-letras.component.css',
+  animations: [
+    trigger('transitionMessages', [
+      state('void', style({ opacity: 0 })),
+      state('enter', style({ opacity: 1 })),
+      transition('void => enter', [
+        animate('300ms ease-in')
+      ]),
+      transition('enter => void', [
+        animate('300ms ease-out')
+      ])
+    ])
+  ]
 })
+
+
+
 export class DetailLetrasComponent {
 
+
+
+  
+  readonly panelOpenState = signal(false);
+
+
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent,  {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // Usa provideHttpClient en lugar de HttpClientModule
+    provideHttpClient(), // Usa provideHttpClient en lugar de HttpClientModule
+    importProvidersFrom(BrowserAnimationsModule) // Provide BrowserAnimationsModule here
+
   ]
 }).catch((err) => console.error(err));
 
