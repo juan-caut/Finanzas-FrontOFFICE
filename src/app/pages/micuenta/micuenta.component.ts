3e2d5b5947898506a883fd77cf86b9e52c044fb1
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-11-11 20:53:44 -0500

    inst

--- src/app/pages/micuenta/micuenta.component.ts
+++ src/app/pages/micuenta/micuenta.component.ts
@@ -10,4 +10,12 @@ import { Component } from '@angular/core';
 export class MicuentaComponent {
 
   selectedStatus: string = 'Mi cuenta';
+
+  username:string='';
+  
+  ngOnInit(): void {
+    const userData = JSON.parse(sessionStorage.getItem('userData') || '{}');
+    console.log('esto es la cartera', userData);
+    this.username=userData.username;
+  }
 }
