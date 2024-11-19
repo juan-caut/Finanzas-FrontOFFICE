2d23528c7961135e0915555c0e4d15e663a733b7
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-11-18 23:52:03 -0500

    8.0v

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
-export const environment = {
-  production: true,
-  base: '/api',
+
+export const environment={
+production:true,
+  base:"http://34.44.231.73:8080"
   //desmarcar y npm run server
   //CORS
-};
+
+
+}
+
